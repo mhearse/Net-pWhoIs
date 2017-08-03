87ba35fee1ca0acec0dfac90aa38d1c04e121e70
Matt Hersant <mhersant@Matthews-MacBook-Pro.local>
2017-08-03 14:38:34 -0700

    Added alt constructor example

--- example.pl
+++ example.pl
@@ -4,6 +4,9 @@ use Net::pWhoIs;
 
 my %attrs = ( req => '166.70.12.30' );
 my $obj = Net::pWhoIs->new(\%attrs);
+
+# Or my $obj = Net::pWhoIs->new({req => '166.70.12.30'});
+
 my $output = $obj->pwhois();
 
 use Data::Dumper;
